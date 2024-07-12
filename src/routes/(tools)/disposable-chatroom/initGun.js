1a32794cef9df050b6643c33c880bacb48f3756e
navneet <navneetsharma2302@gmail.com>
2024-07-12 22:52:45 +0530

    fixed peer relay

--- src/routes/(tools)/disposable-chatroom/initGun.js
+++ src/routes/(tools)/disposable-chatroom/initGun.js
@@ -8,10 +8,10 @@ if (process.env.NODE_ENV === "development") {
 } else {
   peers = [
     // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
-    "https://www.raygun.live/gun",
-    "https://gunmeetingserver.herokuapp.com/gun",
-    "https://gun-us.herokuapp.com/gun",
-    "https://gun-eu.herokuapp.com/gun",
+    "https://gun-manhattan.herokuapp.com/gun",
+    "https://peer.wallie.io/gun",
+    "https://gundb-relay-mlccl.ondigitalocean.app/gun",
+    "https://plankton-app-6qfp3.ondigitalocean.app",
   ];
 }
 
